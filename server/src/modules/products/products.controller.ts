6da78d059181ef852bd724b2998244ccf8e9f63b
rawalabhinav <abhinavrawal2002@gmail.com>
2023-07-26 04:13:36 +0530

    Added mock data

---
+++ server/src/modules/products/products.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import products, {Product} from 'src/products';
+
+@Controller('products')
+export class ProductsController {
+  constructor() {}
+
+  @Get()
+  async index(): Promise<Product[]> {
+    return products;
+  }
+}
