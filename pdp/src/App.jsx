94d9776e9bde04f848a3bc62b2cebeb7a595bab6
rawalabhinav <abhinavrawal2002@gmail.com>
2023-07-25 23:57:20 +0530

    Rollback

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -8,13 +8,9 @@ import Header from "home/Header";
 import Footer from "home/Footer";
 
 const App = () => {
-    const [showHeader, setShowHeader] = useState(false);
     return (
     <div className="text-3xl mx-auto max-w-6xl">
-        {showHeader && <Suspense fallback={<div>Loading header</div>}>
-            <Header />
-        </Suspense>}
-        <button className="text-3xl p-3" onClick={() => setShowHeader(true)}>Click to show header</button>
+        <Header />
         <div className="my-10"> PDP page content </div>
         <Footer />
     </div>)
